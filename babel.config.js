388984c2fd768bddef8a25e1042688bff2e0dc21
Maria Luisa <maria.luisa.alves214@gmail.com>
2024-06-01 18:19:44 -0300

    fix: ajusta arquitetura de pasta

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    "plugins": [
+      [
+      "module-resolver", {
+        "root": ["./src"],
+        "alias": {
+          "@assets": "./scr/assests",
+          "@components": "./scr/components",
+          "@routes":"./src/routes",
+          "@storage":"./src/storage",
+          "@screens": "./scr/screens",
+          "@theme": "./scr/theme",
+          "@utils": "./scr/utils",
+        }
+      }]]
+    
   };
 };
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export function Group(){
+    return (
+        <View>
+            <Text>Group</Text>
+        </View>
+    )
+
+}
